badd0d32bd9157ed0b5682589089201f83077e30
Mael BILLAUD <billaud.mael@gmail.com>
2022-12-09 11:07:31 +0100

    FilterStyle done

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,14 @@
 import { Button } from '@mantine/core';
 import { IconFilter } from "@tabler/icons";
+import "../styles/Filter.css"
 
 function Filter() {
     return(
-        <Button leftIcon={<IconFilter/>} variant="subtle" size="xl">Filters</Button>
+        // <Button leftIcon={<IconFilter/>} variant="subtle" size="xl">Filters</Button>
+        <div id='filter'>
+            <IconFilter size={30} color="#339AF0" id='filter-icon'/>
+            <Button id='filter-button'>Filters</Button>
+        </div>
     )
 }
 
