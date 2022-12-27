ff82cae80b0ad4acc7810e69ef4308b324c0d5e2
Mael BILLAUD <billaud.mael@gmail.com>
2022-12-27 19:21:58 +0100

    Add slider + button "Apply filters"

--- src/components/ParkList.js
+++ src/components/ParkList.js
@@ -1,4 +1,4 @@
-import { Checkbox, Switch } from "@mantine/core";
+import { Checkbox, Switch, Slider, Button } from "@mantine/core";
 import { useState } from "react";
 import "../styles/ParkList.css";
 import Marker from "./Maker.js";
@@ -7,6 +7,15 @@ function ParkList({ markers, setMarkers }) {
   const [equipmentList, setEquipmentList] = useState([]);
   const [isCovered, setCovered] = useState(false);
   const [isVerified, setVerified] = useState(false);
+  const [useDistance, setUseDistance] = useState(
+    "Par rapport à la géo-localisation"
+  );
+  const [distance, setDistance] = useState(0);
+
+  /**
+   * Function qui va appliquer les filtres utilisateurs
+   */
+  function applyFilters() {}
 
   return (
     <div id="container">
@@ -31,16 +40,49 @@ function ParkList({ markers, setMarkers }) {
       </div>
       <div id="switch">
         <Switch
-          checked={isCovered}
-          onChange={(event) => setCovered(event.currentTarget.checked)}
-          label="Parc couvert"
-        />
-        <Switch
           checked={isVerified}
-          onChange={(event) => setVerified(event.currentTarget.checked)}
+          onChange={(event) => {
+            setVerified(event.currentTarget.checked);
+          }}
           label="Parc vérifié"
           description="Vérifié par au moins 5 utilisateurs"
         />
+        <Switch
+          checked={isCovered}
+          onChange={(event) => {
+            setCovered(event.currentTarget.checked);
+          }}
+          label="Parc couvert"
+        />
+      </div>
+      <div>
+        <Switch
+          checked={useDistance}
+          onChange={(event) => {
+            setUseDistance(event.currentTarget.checked);
+          }}
+          label={useDistance ? `Distance  - ${distance}km` : "Distance"}
+          description={useDistance ? `` : `Par rapport à la géo-localisation`}
+        />
+        {useDistance && (
+          <Slider
+            value={distance}
+            onChange={setDistance}
+            size="sm"
+            marks={[
+              { value: 25, label: "25km" },
+              { value: 50, label: "50km" },
+              { value: 75, label: "75km" },
+            ]}
+            id="slider"
+            label={null}
+          />
+        )}
+      </div>
+      <div id="btn-appliquer">
+        <Button radius="xl" compact onClick={applyFilters}>
+          Appliquer les filtres
+        </Button>
       </div>
       <div id="content">
         {markers.map((value, index) => {
