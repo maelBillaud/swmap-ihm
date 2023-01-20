3f4d57bd5aa550c8dcb679467cf36a8b7e811555
Mael BILLAUD <billaud.mael@gmail.com>
2023-01-20 09:58:21 +0100

    fix gitignore

---
+++ src/environments/environment.js
@@ -0,0 +1,7 @@
+export default function getMapBoxAccessToken() {
+  return "pk.eyJ1IjoibWFlbGIxIiwiYSI6ImNsYjU2aDN3aDAzanczcHF1aWN3Mnh0Y2kifQ.h1dHrk1quc7T4W-nSI5yWQ";
+}
+
+export function getGeoApiFyApiKey() {
+  return "e1285231a71a46ce8719ebc9d1db5f4b";
+}
